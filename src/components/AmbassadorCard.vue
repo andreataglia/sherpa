e9d0821787239a91b5e9af1faa2f08737bd0c860
andreataglia <drew.taglia@gmail.com>
2020-02-26 11:43:43 +0100

    added Paolo info. ambassador profil is now squared

--- src/components/AmbassadorCard.vue
+++ src/components/AmbassadorCard.vue
@@ -6,7 +6,7 @@
           :src="`${publicPath}img/ambassadorPics/amb-${this.id}.jpg`"
           class="white--text align-end"
           gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-          height="200px"
+          height="300"
         >
           <v-card-title v-text="getAmbassadorById.name"></v-card-title>
         </v-img>
