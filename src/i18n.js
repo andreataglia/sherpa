1077349febff0ea7b086bbc4d53972f4536314ce
andreataglia <drew.taglia@gmail.com>
2020-06-24 23:03:22 +0200

    selected init lang based on navigator object

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,38 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import getBrowserLocale from '@/locales/get-browser-locale';
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
-function loadLocaleMessages () {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+function loadLocaleMessages() {
+  const locales = require.context(
+    './locales',
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i
+  );
+  const messages = {};
+  locales.keys().forEach((key) => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
+      const locale = matched[1];
+      messages[locale] = locales(key);
     }
-  })
-  return messages
+  });
+  return messages;
+}
+
+function getStartingLocale() {
+  const browserLocale = getBrowserLocale({ countryCodeOnly: true });
+
+  if (browserLocale.includes('it')) {
+    return 'it';
+  } else {
+    return process.env.VUE_APP_I18N_LOCALE || 'en';
+  }
 }
 
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  locale: getStartingLocale(),
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages()
-})
+  messages: loadLocaleMessages(),
+});
